2de52c98039d6d79b18b7f0a71bc74ce4fc445d1
Aiman Haikal <aimanhaikal013@gmail.com>
2024-05-24 19:03:44 +0800

    Add GET TimeSlot

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -49,19 +49,49 @@ class MovieController extends Controller
     //GET TimeSlot
     public function getMoviesByTimeslot(Request $request)
     {
+        // Remove quotes from the input data
+        $theaterName = trim($request->input('theater_name'), '"');
+        $timeStart = trim($request->input('time_start'), '"');
+        $timeEnd = trim($request->input('time_end'), '"');
+
+        $request->merge([
+            'theater_name' => $theaterName,
+            'time_start' => $timeStart,
+            'time_end' => $timeEnd,
+        ]);
+
         $request->validate([
             'theater_name' => 'required|string',
-            'time_start' => 'required|date_format:H:i',
-            'time_end' => 'required|date_format:H:i',
+            'time_start' => 'required|date_format:Y-m-d H:i:s',
+            'time_end' => 'required|date_format:Y-m-d H:i:s',
         ]);
 
         $theaterName = $request->input('theater_name');
         $timeStart = $request->input('time_start');
         $timeEnd = $request->input('time_end');
 
-        $movies = Movie::where('theater_name', $theaterName)
-            ->whereTime('showtime_start', '>=', $timeStart)
-            ->whereTime('showtime_end', '<=', $timeEnd)
+        $movies = Movie::select(
+            'Movie.movie_list.movie_id',
+            'title',
+            'duration',
+            'views',
+            'genre',
+            'poster',
+            'overall_rating',
+            'theater_name',
+            'start_time',
+            'end_time',
+            'description',
+            'theater_room_no'
+        )
+            ->join('Movie.screenings', 'Movie.movie_list.movie_id', '=', 'Movie.screenings.movie_id')
+            ->join('Movie.theaters', 'Movie.screenings.theater_id', '=', 'Movie.theaters.theater_id')
+            ->where('Movie.theaters.theater_name', $theaterName)
+            ->where(function ($query) use ($timeStart, $timeEnd) {
+                $query->where('screenings.start_time', '>=', $timeStart)
+                    ->where('screenings.end_time', '<=', $timeEnd);
+            })
+            ->orderBy('Movie.movie_list.movie_id')
             ->get();
 
         return response()->json($movies);
--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,10 @@ use App\Http\Controllers\MovieController;
 
 Route::prefix('movies')->group(function () {
     Route::get('/', [MovieController::class, 'index']);
-    Route::get('/{id}', [MovieController::class, 'show']);
+    Route::get('/show/{id}', [MovieController::class, 'show']);
     Route::post('/', [MovieController::class, 'store']);
-    Route::put('/{id}', [MovieController::class, 'update']);
-    Route::delete('/{id}', [MovieController::class, 'destroy']);
+    Route::put('/update/{id}', [MovieController::class, 'update']);
+    Route::delete('/destroy/{id}', [MovieController::class, 'destroy']);
     Route::get('/genre/{genre}', [MovieController::class, 'getMoviesByGenre']);
     Route::get('/timeslot', [MovieController::class, 'getMoviesByTimeslot']);
 });
